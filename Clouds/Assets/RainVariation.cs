79c2320e1c7f636ed5ff72b3765fbd17b6d44bbc
John Buchaniec <John.Buchaniec@iam.local>
2019-11-06 18:26:30 -0600

    Project done!

---
+++ Clouds/Assets/RainVariation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainVariation : MonoBehaviour
+{
+
+    ParticleSystem Rain;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rain = gameObject.GetComponent<ParticleSystem>();
+        Rain.enableEmission = true;
+        Rain.emissionRate = 150;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float RainRate = 10f;
+        float valToBeLerped = 150;
+        valToBeLerped = Mathf.Lerp(150, 650, RainRate);
+    }
+}
